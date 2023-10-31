55fd3834ac8885575e83fc646c7734f4ce7d84c7
Marquardt Snell <thefabulouscube@gmail.com>
2023-10-30 22:18:01 -0400

    Add project files.

---
+++ Net Mock API test/CatSchemas.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace Net_Mock_API_test
+{
+    public class CatBreeds
+    {
+    public string? breed { get; set; }
+        public string? country { get; set; }
+    public string? origin { get; set; }
+    public string? coat { get; set; }
+    public string? pattern { get; set; }
+    }
+
+    public class CatFacts
+    { public string? fact { get; set; }
+        public int length { get; set; } 
+    }
+}
---
+++ Net Mock API test/Controllers/CatsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Net_Mock_API_test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatsController : Controller
+    {
+        private readonly IConfiguration _config;
+        public CatsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet("GetCatFact")]
+        public async Task<string> GetAsync()
+        {
+            string baseurl = _config["CatsUrl"];
+            string fact = "SUT - ";
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(baseurl + "/fact"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    var thing = JsonConvert.DeserializeObject<CatFacts>(apiResponse);
+                    fact += thing.fact;
+                }
+
+            }
+            return fact;
+        }
+    }
+}
